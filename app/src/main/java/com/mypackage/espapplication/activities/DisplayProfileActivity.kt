8547ba91e9ada001950f097f2713a1e719654386
therake77 <jarema9125@hotmail.com>
2024-11-25 21:22:44 -0500

    this is the final project

--- app/src/main/java/com/mypackage/espapplication/activities/DisplayProfileActivity.kt
+++ app/src/main/java/com/mypackage/espapplication/activities/DisplayProfileActivity.kt
@@ -7,13 +7,13 @@ import android.widget.Button
 import android.widget.EditText
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Text
+
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.mypackage.espapplication.R
 import com.mypackage.espapplication.models.Parameter
 import com.mypackage.espapplication.models.Profile
-import org.w3c.dom.Text
+
 import java.util.Locale
 
 class DisplayProfileActivity : ComponentActivity() {
@@ -35,19 +35,6 @@ class DisplayProfileActivity : ComponentActivity() {
     private lateinit var deleteButton: Button
     private lateinit var modifyButton: Button
 
-    private lateinit var textWatcher1 : TextWatcher
-    private lateinit var textWatcher2 : TextWatcher
-    private lateinit var textWatcher3 : TextWatcher
-    private lateinit var textWatcher4 : TextWatcher
-    private lateinit var textWatcher5 : TextWatcher
-    private lateinit var textWatcher6 : TextWatcher
-    private lateinit var textWatcher7 : TextWatcher
-    private lateinit var textWatcher8 : TextWatcher
-    private lateinit var textWatcher9 : TextWatcher
-    private lateinit var textWatcher10 : TextWatcher
-    private lateinit var textWatcher11 : TextWatcher
-    private lateinit var textWatcher12 : TextWatcher
-    private lateinit var textWatcher13 : TextWatcher
 
     private lateinit var profileSelected : Profile
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -57,32 +44,20 @@ class DisplayProfileActivity : ComponentActivity() {
         profileSelected = intent.extras!!.get("profileSelected") as Profile
         initAll()
         setValues()
-        deleteButton.setOnClickListener({
+        deleteButton.setOnClickListener{
             dbRefToProf.child(profileSelected.id!!).removeValue()
             finish()
-        })
-        modifyButton.setOnClickListener({
-            dbRefToProf.child(profileSelected.id!!).setValue(Profile(profileSelected.id,nameField.text.toString(),Parameter(alcoholMaxField.toString().toDouble(),alcoholMinField.toString().toDouble()),
-                Parameter(altitudMaxField.toString().toDouble(),altitudMinField.toString().toDouble()),
-                Parameter(co2MaxField.toString().toDouble(),co2MinField.toString().toDouble()),
-                Parameter(humedadMaxField.toString().toDouble(),humedadMinField.toString().toDouble()),
-                Parameter(presionlMaxField.toString().toDouble(), presionMinField.toString().toDouble()),
-                Parameter(temperaturaMaxField.toString().toDouble(),temperaturaMinField.toString().toDouble())))
+        }
+        modifyButton.setOnClickListener{
+            val profileToModify = Profile(profileSelected.id,nameField.text.toString(),Parameter(alcoholMaxField.text.toString().toDouble(),alcoholMinField.text.toString().toDouble()),
+                Parameter(altitudMaxField.text.toString().toDouble(),altitudMinField.text.toString().toDouble()),
+                Parameter(co2MaxField.text.toString().toDouble(),co2MinField.text.toString().toDouble()),
+                Parameter(humedadMaxField.text.toString().toDouble(),humedadMinField.text.toString().toDouble()),
+                Parameter(presionlMaxField.text.toString().toDouble(), presionMinField.text.toString().toDouble()),
+                Parameter(temperaturaMaxField.text.toString().toDouble(),temperaturaMinField.text.toString().toDouble()))
+            dbRefToProf.child(profileSelected.id!!).setValue(profileToModify)
             finish()
-        })/*
-        textWatcher1 = setListenerForEdit(nameField)
-        textWatcher2 = setListenerForEdit(alcoholMaxField)
-        textWatcher3 = setListenerForEdit(alcoholMinField)
-        textWatcher4 = setListenerForEdit(altitudMaxField)
-        textWatcher5 = setListenerForEdit(altitudMinField)
-        textWatcher6 = setListenerForEdit(co2MaxField)
-        textWatcher7 = setListenerForEdit(co2MinField)
-        textWatcher8 = setListenerForEdit(humedadMaxField)
-        textWatcher9 = setListenerForEdit(humedadMinField)
-        textWatcher10 = setListenerForEdit(presionlMaxField)
-        textWatcher11 = setListenerForEdit(presionMinField)
-        textWatcher12 = setListenerForEdit(temperaturaMaxField)
-        textWatcher13 = setListenerForEdit(temperaturaMinField)*/
+        }
         setListenerForEdit(nameField)
         setListenerForEdit(alcoholMaxField)
         setListenerForEdit(alcoholMinField)
@@ -100,56 +75,39 @@ class DisplayProfileActivity : ComponentActivity() {
     private fun setListenerForEdit(editText: EditText) {
         editText.addTextChangedListener(object : TextWatcher{
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                TODO("Not yet implemented")
+                return
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                //modifyButton.visibility = Button.VISIBLE
+                modifyButton.visibility = Button.VISIBLE
             }
-
             override fun afterTextChanged(s: Editable?) {
-                TODO("Not yet implemented")
-                //deleteAllListeners()
+                return
             }
         })
 
     }
 
-    private fun deleteAllListeners() {
-        nameField.removeTextChangedListener(textWatcher1)
-        alcoholMaxField.removeTextChangedListener(textWatcher2)
-        alcoholMinField.removeTextChangedListener(textWatcher3)
-        altitudMaxField.removeTextChangedListener(textWatcher4)
-        altitudMinField.removeTextChangedListener(textWatcher5)
-        co2MaxField.removeTextChangedListener(textWatcher6)
-        co2MinField.removeTextChangedListener(textWatcher7)
-        humedadMaxField.removeTextChangedListener(textWatcher8)
-        humedadMinField.removeTextChangedListener(textWatcher9)
-        presionlMaxField.removeTextChangedListener(textWatcher10)
-        presionMinField.removeTextChangedListener(textWatcher11)
-        temperaturaMaxField.removeTextChangedListener(textWatcher12)
-        temperaturaMinField.removeTextChangedListener(textWatcher13)
-    }
 
     private fun setValues() {
-        nameField.hint = profileSelected.name
-        alcoholMaxField.hint = String.format(Locale.ENGLISH,"%f",profileSelected.alcohol.maxValue)
-        alcoholMinField.hint = String.format(Locale.ENGLISH,"%f",profileSelected.alcohol.minValue)
+        nameField.setText(profileSelected.name)
+        alcoholMaxField.setText(String.format(Locale.ENGLISH,"%f",profileSelected.alcohol.maxValue))
+        alcoholMinField.setText(String.format(Locale.ENGLISH,"%f",profileSelected.alcohol.minValue))
 
-        altitudMaxField.hint = String.format(Locale.ENGLISH,"%f",profileSelected.altitud.maxValue)
-        altitudMinField.hint = String.format(Locale.ENGLISH,"%f",profileSelected.altitud.minValue)
+        altitudMaxField.setText(String.format(Locale.ENGLISH,"%f",profileSelected.altitud.maxValue))
+        altitudMinField.setText(String.format(Locale.ENGLISH,"%f",profileSelected.altitud.minValue))
 
-        co2MaxField.hint = String.format(Locale.ENGLISH,"%f",profileSelected.co2.maxValue)
-        co2MinField.hint = String.format(Locale.ENGLISH,"%f",profileSelected.co2.minValue)
+        co2MaxField.setText(String.format(Locale.ENGLISH,"%f",profileSelected.co2.maxValue))
+        co2MinField.setText(String.format(Locale.ENGLISH,"%f",profileSelected.co2.minValue))
 
-        humedadMaxField.hint = String.format(Locale.ENGLISH,"%f",profileSelected.humedaddesuelo.maxValue)
-        humedadMinField.hint = String.format(Locale.ENGLISH,"%f",profileSelected.humedaddesuelo.minValue)
+        humedadMaxField.setText(String.format(Locale.ENGLISH,"%f",profileSelected.humedaddesuelo.maxValue))
+        humedadMinField.setText(String.format(Locale.ENGLISH,"%f",profileSelected.humedaddesuelo.minValue))
 
-        presionlMaxField.hint = String.format(Locale.ENGLISH,"%f",profileSelected.presion.maxValue)
-        presionMinField.hint = String.format(Locale.ENGLISH,"%f",profileSelected.presion.minValue)
+        presionlMaxField.setText(String.format(Locale.ENGLISH,"%f",profileSelected.presion.maxValue))
+        presionMinField.setText(String.format(Locale.ENGLISH,"%f",profileSelected.presion.minValue))
 
-        temperaturaMaxField.hint = String.format(Locale.ENGLISH,"%f",profileSelected.temperatura.maxValue)
-        temperaturaMinField.hint = String.format(Locale.ENGLISH,"%f",profileSelected.temperatura.minValue)
+        temperaturaMaxField.setText(String.format(Locale.ENGLISH,"%f",profileSelected.temperatura.maxValue))
+        temperaturaMinField.setText(String.format(Locale.ENGLISH,"%f",profileSelected.temperatura.minValue))
     }
 
     private fun initAll() {
